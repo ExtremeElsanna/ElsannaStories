1e6708c6f3ec911121736629aad6379bf0db420f
ExtremeElsanna <Apples292@hotmail.co.uk>
2015-08-10 23:01:08 +0100

    Features

--- test.php
+++ test.php
@@ -1,25 +1,4 @@
-﻿<?php include("/hdd/elsanna-ssl/scripts/utf8Headers.php"); ?>
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>Elsanna Stories</title>
-		<meta http-equiv='Content-Type' content='text/html; charset=utf-8'>
-	</head>
-	<body>
-		<table>
-			<tr><th>Title</th></tr>
-			<?php
-				include("/hdd/database-config/config.php");
-				$pdo = new PDO('mysql:host='.$DBhost.';dbname='.$DBname, $DBusername, $DBpassword);
-
-				$stmt = $pdo->prepare('SELECT Id,Title FROM Stories WHERE Id = 131');
-				$stmt->execute();
-				$rows = $stmt->fetchAll();
-				foreach ($rows as $row) {
-					echo '<tr><td><a href="story/?id='.$row['Id'].'">'.$row['Title'].'</a></td></tr>';
-				}
-			?>
-
-		</table>
-	</body>
-</html>
+﻿<?php
+$letter = "é";
+echo "é";
+?>
