8c47caddd1d06a7d909e99e384d17630bcfdae83
ExtremeElsanna <Apples292@hotmail.co.uk>
2015-08-11 13:35:24 +0100

    Activation Debug

--- register/index.php
+++ register/index.php
@@ -6,7 +6,7 @@
 		<meta http-equiv='Content-Type' content='text/html; charset=utf-8'>
 	</head>
 	<body>
-		<form action="genpass.php" method="post">
+		<form action="register.php" method="post">
 			<input type="text" name="user" value="" placeholder="Username">
 			<input type="password" name="password" value="" placeholder="Password">
 			<input type="text" name="email" value="" placeholder="Email">
