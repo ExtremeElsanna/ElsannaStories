22dff629011188f301eba6c882d68c938d0dc85b
ExtremeElsanna <Apples292@hotmail.co.uk>
2015-08-11 14:36:34 +0100

    Activation Debug

--- activate/index.php
+++ activate/index.php
@@ -5,15 +5,21 @@
 		$stmt = $pdo->prepare("SET NAMES 'utf8'");
 		$stmt->execute();
 		
-		$stmt = $pdo->prepare('SELECT UserId FROM AccountActivation WHERE ActivationCode = :code');
+		$stmt = $pdo->prepare('SELECT AccountActivationId,UserId FROM AccountActivation WHERE ActivationCode = :code');
 		$stmt->bindParam(':code', $_GET['code'], PDO::PARAM_STR); // <-- Automatically sanitized for SQL by PDO
 		$stmt->execute();
 		$row = $stmt->fetch();
+		$activationId = $row['AccountActivationId'];
 		$userId = $row['UserId'];
 		
 		$stmt = $pdo->prepare('UPDATE Users SET Activated = 1 WHERE Id = :id');
-		$stmt->bindParam(':id', $userId, PDO::PARAM_STR); // <-- Automatically sanitized for SQL by PDO
+		$stmt->bindParam(':id', $userId, PDO::PARAM_INT); // <-- Automatically sanitized for SQL by PDO
 		$stmt->execute();
+		
+		$stmt = $pdo->prepare('DELETE FROM AccountActivation WHERE AccountActivationId = :id');
+		$stmt->bindParam(':id', activationId, PDO::PARAM_INT); // <-- Automatically sanitized for SQL by PDO
+		$stmt->execute();
+		
 		
 		echo 'Activated Account UserId: '.$userId;
 	}
