9bfcf19599920da16013d346aa3b133bbaf32748
ExtremeElsanna <Apples292@hotmail.co.uk>
2015-08-11 13:29:42 +0100

    Activation Debug

---
+++ register/index.php
@@ -0,0 +1,16 @@
+﻿<?php include("/hdd/elsanna-ssl/scripts/utf8Headers.php"); ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Elsanna Stories</title>
+		<meta http-equiv='Content-Type' content='text/html; charset=utf-8'>
+	</head>
+	<body>
+		<form action="genpass.php" method="post">
+			<input type="text" name="user" value="" placeholder="Username">
+			<input type="password" name="password" value="" placeholder="Password">
+			<input type="text" name="email" value="" placeholder="Email">
+			<input type="submit" name="submit" value="Login">
+		</form>
+	</body>
+</html>
