9c69ef5ee3d2545e9bfee40cc178a97e7592c8e8
ExtremeElsanna <Apples292@hotmail.co.uk>
2015-08-11 21:04:30 +0100

    Added ReCaptcha

--- register/register.php
+++ register/register.php
@@ -89,7 +89,7 @@
 	echo $_POST['g-recaptcha-response'];
 	$captcha = $_POST['g-recaptcha-response'];
 	$response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$config['RcaptchaSecretKey']."&response=".$captcha."&remoteip=".$_SERVER['REMOTE_ADDR']);
-	print_r(json_decode ( $response));
+	print(json_decode ( $response)['success']);
 	if ($response.success == true) {
 		//$pdo = new PDO('mysql:host='.$config['DBhost'].';dbname='.$config['DBname'], $config['DBusername'], $config['DBpassword'], $config['DBoptions']);
 		//$userId = generateUser($config,$pdo);
