1f0ae2813b9c839848381f18f825741c4bf01001
ExtremeElsanna <Apples292@hotmail.co.uk>
2015-08-14 20:12:13 +0100

    Search Engine Debug

--- search.php
+++ search.php
@@ -79,11 +79,9 @@ for ($i = $wordcount; $i > 0; $i--) {
 			}
 			$split = substr($split,1);
 			$persplitcount = substri_count($row[1],$split);
-			echo "I'm here!";
 			if ($debug == True) {
 				echo '"'.$split.'" - '.$persplitcount." and ";
 			}
-			echo "I'm here!";
 			if ($persplitcount == 0) {
 				$no_results_found = True;
 			}
@@ -97,6 +95,7 @@ for ($i = $wordcount; $i > 0; $i--) {
 				echo "Total ".$hitCounts;
 				echo "<br><br>";
 			}
+			echo "I'm here!";
 		}
 	}
 }
