1be2de7e6fb7b658602bc6835aca6d7ec082ac7f
ExtremeElsanna <Apples292@hotmail.co.uk>
2015-08-14 20:38:04 +0100

    Started Search Engine Anew according to 'search engine algorithm.txt'

--- search.php
+++ search.php
@@ -6,4 +6,12 @@ function mb_substri_count($haystack, $needle) {
 $dummySearch = $_GET['search'];
 $row = array(0 => 1,
 			 1 => $_GET['story']);
+
+# Array containing each word of the query
+$queryWords = explode(" ", $dummySearch);
+
+# For each 'split size' e.g. not splitting the query at all is 0 while splitting it once is 1. See search engine algorithm.txt
+for ($splitCount = 0; $i <= count($queryWords)-1; $i++) {
+	echo 'Making '.$splitCount.' splits!<br>';
+}
 ?>
