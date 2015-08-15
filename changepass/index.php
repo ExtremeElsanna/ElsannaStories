4c655340d386b42d48a7eceb4966caad5eea9a79
ExtremeElsanna <Apples292@hotmail.co.uk>
2015-08-15 18:32:55 +0100

    Started adding changing username

---
+++ changepass/index.php
@@ -0,0 +1,18 @@
+<?php
+include("/hdd/elsanna-ssl/scripts/utf8Headers.php");
+include("/hdd/elsanna-ssl/scripts/sessionHandler.php");
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Elsanna Stories</title>
+		<meta http-equiv='Content-Type' content='text/html; charset=utf-8'>
+	</head>
+	<body>
+		<?php
+			$headerRefer = "/";
+			include("/hdd/elsanna-ssl/classes/header.php");
+		?>
+		
+	</body>
+</html>
