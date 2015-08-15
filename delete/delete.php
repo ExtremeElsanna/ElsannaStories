ed377a7ff483e181bf15706ebc30a39d564e02c0
ExtremeElsanna <Apples292@hotmail.co.uk>
2015-08-15 11:16:21 +0100

    Delete Account debug

--- delete/delete.php
+++ delete/delete.php
@@ -1,5 +1,16 @@
 <?php
 	include("/hdd/elsanna-ssl/scripts/sessionHandler.php");
 	include("/hdd/config/config.php");
+	if ($_SESSION['loggedIn'] != 1) {
+		# Not logged in
+		header("Location: /");
+		die();
+	}
+	if (!isset($_SERVER['HTTP_REFERER'])) {
+		# Not referred by /delete/
+		header("Location: /user/".$_SESSION['username']);
+		die();
+	}
+	print_r($_SERVER);
 	print($_SERVER['HTTP_REFERER']);
 ?>
