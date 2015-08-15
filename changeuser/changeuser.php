830fac305ec45b439f45af2cb221105153b44782
ExtremeElsanna <Apples292@hotmail.co.uk>
2015-08-15 21:14:22 +0100

    Change Username Finished

--- changeuser/changeuser.php
+++ changeuser/changeuser.php
@@ -25,7 +25,9 @@
 	$stmt->execute();
 	$row = $stmt->fetch();
 	if ($row['Id'] == "") {
+		// Username not taken
 		if (preg_match("/(?:.*[^abcdefghijklmnopqrstuvwxyz0123456789].*)+/i",$newUser) == 0) {
+			// Username contains valid chars
 			$stmt = $pdo->prepare("UPDATE Users SET Username = :newUser WHERE Id = :id");
 			$stmt->bindParam(':newUser', $newUser, PDO::PARAM_STR); // <-- Automatically sanitized for SQL by PDO
 			$stmt->bindParam(':id', $userId, PDO::PARAM_INT); // <-- Automatically sanitized for SQL by PDO
@@ -34,6 +36,8 @@
 			$stmt->bindParam(':newUpperUser', $newUpperUser, PDO::PARAM_STR); // <-- Automatically sanitized for SQL by PDO
 			$stmt->bindParam(':id', $userId, PDO::PARAM_INT); // <-- Automatically sanitized for SQL by PDO
 			$stmt->execute();
+			header("Location: /");
+			die();
 		} else {
 			// Username contains invalid chars
 			header("Location: /changeuser/");
