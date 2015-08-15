11740f25310187137f6495ce6513b61afa30209f
ExtremeElsanna <Apples292@hotmail.co.uk>
2015-08-15 11:55:39 +0100

    Added forbidden page

---
+++ error/forbidden.php
@@ -0,0 +1,26 @@
+<?php
+include("/hdd/elsanna-ssl/scripts/utf8Headers.php");
+include("/hdd/elsanna-ssl/scripts/sessionHandler.php");
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Elsanna Stories</title>
+		<meta http-equiv='Content-Type' content='text/html; charset=utf-8'>
+	</head>
+	<body>
+		<?php
+			$headerRefer = '/register/';
+			include("/hdd/elsanna-ssl/classes/header.php");
+		?>
+		
+		<form action="register.php" method="post">
+			<input type="text" name="user" value="" placeholder="Username">
+			<input type="password" name="password" value="" placeholder="Password">
+			<input type="password" name="password_confirm" value="" placeholder="Password Confirmation">
+			<input type="text" name="email" value="" placeholder="Email">
+			<div class="g-recaptcha" data-sitekey="<?php echo $config['RcaptchaSiteKey'] ?>"></div>
+			<input type="submit" value="Register">
+		</form>
+	</body>
+</html>
