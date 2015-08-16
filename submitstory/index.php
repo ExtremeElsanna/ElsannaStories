0dfb6f195336ed28020a3e8efcfd0e2228452238
ExtremeElsanna <Apples292@hotmail.co.uk>
2015-08-16 21:03:16 +0100

    Submit story debug

--- submitstory/index.php
+++ submitstory/index.php
@@ -1,6 +1,11 @@
 ﻿<?php
 include("/hdd/elsanna-ssl/scripts/utf8Headers.php");
 include("/hdd/elsanna-ssl/scripts/sessionHandler.php");
+if ($_SESSION['loggedIn'] != 1) {
+	// Not logged in
+	header("Location: /login/?refer=/submitstory/");
+	die();
+}
 ?>
 <!DOCTYPE html>
 <html>
