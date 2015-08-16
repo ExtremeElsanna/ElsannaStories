78e3b7b7341adcd7d023ec20a838dc382c5563a1
ExtremeElsanna <Apples292@hotmail.co.uk>
2015-08-16 14:30:30 +0100

    Submit story debug

--- submitstory/index.php
+++ submitstory/index.php
@@ -17,58 +17,73 @@ include("/hdd/elsanna-ssl/scripts/sessionHandler.php");
 		?>
 		
 		<form action="submit.php" method="post">
+			Title<br>
 			<input type="text" name="Title" value="" placeholder="Title"><br>
-			
+			<br>
+			Author<br>
 			<input type="text" name="Author" value="" placeholder="Author"><br>
-			
+			<br>
+			Length (words)<br>
 			<input type="number" name="Length" value="" min="1"><br>
-			
-			<input type="radio" name="StoryType" value="Multi-Chapter"><br>
-			<input type="radio" name="StoryType" value="One-Shot"><br>
-			<input type="radio" name="StoryType" value="One-Shot Series"><br>
-			
-			<input type="radio" name="Complete" value="Yes"><br>
-			<input type="radio" name="Complete" value="No"><br>
-			<input type="radio" name="Complete" value="Unknown"><br>
-			
-			<input type="radio" name="Setting" value="Canon"><br>
-			<input type="radio" name="Setting" value="Alternate Universe (AU)"><br>
-			<input type="radio" name="Setting" value="Modern Alternate Universe (mAU)"><br>
-			<input type="radio" name="Setting" value="Same Time and Place (STP)"><br>
-			<input type="radio" name="Setting" value="Unknown"><br>
-			
-			<input type="text" name="ElsaCharacter" value="" placeholder="Elsa's Character"><br>
-			
-			<input type="text" name="AnnaCharacter" value="" placeholder="Anna's Character"><br>
-			
-			<input type="radio" name="ElsaPowers" value="Canon"><br>
-			<input type="radio" name="ElsaPowers" value="Different"><br>
-			<input type="radio" name="ElsaPowers" value="None"><br>
-			<input type="radio" name="ElsaPowers" value="Unknown"><br>
-			
-			<input type="radio" name="AnnaPowers" value="No"><br>
-			<input type="radio" name="AnnaPowers" value="Yes"><br>
-			
-			<input type="radio" name="Sisters" value="Yes"><br>
-			<input type="radio" name="Sisters" value="No"><br>
-			<input type="radio" name="Sisters" value="Unknown"><br>
-			
-			<input type="radio" name="Age" value="K"><br>
-			<input type="radio" name="Age" value="K+"><br>
-			<input type="radio" name="Age" value="T"><br>
-			<input type="radio" name="Age" value="M"><br>
-			
-			<input type="radio" name="SmutLevel" value="None"><br>
-			<input type="radio" name="SmutLevel" value="Plot Focused"><br>
-			<input type="radio" name="SmutLevel" value="Light"><br>
-			<input type="radio" name="SmutLevel" value="Medium"><br>
-			<input type="radio" name="SmutLevel" value="Heavy"><br>
-			<input type="radio" name="SmutLevel" value="Pure"><br>
-			
+			<br>
+			Story Type<br>
+			<input type="radio" name="StoryType" value="MC"> Multi-Chapter<br>
+			<input type="radio" name="StoryType" value="OS"> One-Shot<br>
+			<input type="radio" name="StoryType" value="OSS"> One-Shot Series<br>
+			<br>
+			Complete<br>
+			<input type="radio" name="Complete" value="Y"> Yes<br>
+			<input type="radio" name="Complete" value="N"> No<br>
+			<input type="radio" name="Complete" value="U"> Unknown<br>
+			<br>
+			Setting<br>
+			<input type="radio" name="Setting" value="C"> Canon<br>
+			<input type="radio" name="Setting" value="AU"> Alternate Universe (AU)<br>
+			<input type="radio" name="Setting" value="mAU"> Modern Alternate Universe (mAU)<br>
+			<input type="radio" name="Setting" value="STP"> Same Time and Place (STP)<br>
+			<input type="radio" name="Setting" value="U"> Unknown<br>
+			<br>
+			Elsa's Character<br>
+			<input type="text" name="ElsaCharacter" value="" placeholder="Queen"><br>
+			<br>
+			Anna's Character<br>
+			<input type="text" name="AnnaCharacter" value="" placeholder="Princess"><br>
+			<br>
+			Elsa's Powers<br>
+			<input type="radio" name="ElsaPowers" value="C"> Canon<br>
+			<input type="radio" name="ElsaPowers" value="D"> Different<br>
+			<input type="radio" name="ElsaPowers" value="N"> None<br>
+			<input type="radio" name="ElsaPowers" value="U"> Unknown<br>
+			<br>
+			Anna's Powers<br>
+			<input type="radio" name="AnnaPowers" value="N"> No<br>
+			<input type="radio" name="AnnaPowers" value="Y"> Yes<br>
+			<br>
+			Sisters<br>
+			<input type="radio" name="Sisters" value="Y"> Yes<br>
+			<input type="radio" name="Sisters" value="N"> No<br>
+			<input type="radio" name="Sisters" value="U"> Unknown<br>
+			<br>
+			Age [<a href="https://www.fictionratings.com/">X</a>]<br>
+			<input type="radio" name="Age" value="K"> K<br>
+			<input type="radio" name="Age" value="KP"> K+<br>
+			<input type="radio" name="Age" value="T"> T<br>
+			<input type="radio" name="Age" value="M"> M<br>
+			<br>
+			Smut Prominence<br>
+			<input type="radio" name="SmutLevel" value="N"> None<br>
+			<input type="radio" name="SmutLevel" value="PL"> Plot Focused<br>
+			<input type="radio" name="SmutLevel" value="L"> Light<br>
+			<input type="radio" name="SmutLevel" value="M"> Medium<br>
+			<input type="radio" name="SmutLevel" value="H"> Heavy<br>
+			<input type="radio" name="SmutLevel" value="PU"> Pure<br>
+			<br>
+			URL of Story<br>
 			<input type="text" name="Url" value="" placeholder="URL"><br>
-			
+			<br>
+			Date Published<br>
 			<input type="date" name="DatePublished" value="" min="2013-11-27" max="<?php echo $currentDate ?>"><br>
-			
+			<br>
 			<input type="submit" value="Submit">
 		</form>
 	</body>
