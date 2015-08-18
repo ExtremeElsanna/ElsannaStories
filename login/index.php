2ac7b92321f051938a7861c7d55ebde502763ffd
ExtremeElsanna <Apples292@hotmail.co.uk>
2015-08-18 21:35:32 +0100

    Activation email debug

--- login/index.php
+++ login/index.php
@@ -15,7 +15,7 @@ $errors = array(1 => "Account Activated!",
 				8 => "User does not exist.",
 				9 => "Account has already been activated.");
 if (isset($_GET['id']) and is_numeric($_GET['id'])) {
-	$errors[4] = "Account not Activated. <a href='/?id=".$_GET['id']."'>Resend Activation Email</a>";
+	$errors[4] = "Account not Activated. <a href='/resend/?id=".$_GET['id']."'>Resend Activation Email</a>";
 }
 ?>
 <!DOCTYPE html>
