0f6db4ff2a0d37ba84c87cba489f8550a3ae5392
ExtremeElsanna <Apples292@hotmail.co.uk>
2015-09-02 20:53:35 +0100

    Review page system debug

--- story/index.php
+++ story/index.php
@@ -333,19 +333,19 @@ $id = $_GET['id'];
 			}
 			$pageHTML = "\t\t\t<tr><td style='border: 1px solid black' colspan=4>";
 			if ($page > 2) {
-				$pageHTML = $pageHTML + "<a href='?id=".$id."&page=".($page-2)."'>".($page-2)."</a> ";
+				$pageHTML = $pageHTML."<a href='?id=".$id."&page=".($page-2)."'>".($page-2)."</a> ";
 			}
 			if ($page > 1) {
-				$pageHTML = $pageHTML + "<a href='?id=".$id."&page=".($page-1)."'>".($page-1)."</a> ";
+				$pageHTML = $pageHTML."<a href='?id=".$id."&page=".($page-1)."'>".($page-1)."</a> ";
 			}
-			$pageHTML = $pageHTML + $page + " ";
+			$pageHTML = $pageHTML.$page." ";
 			if ($page < $pages) {
-				$pageHTML = $pageHTML + "<a href='?id=".$id."&page=".($page+1)."'>".($page+1)."</a> ";
+				$pageHTML = $pageHTML."<a href='?id=".$id."&page=".($page+1)."'>".($page+1)."</a> ";
 			}
 			if ($page < $pages-1) {
-				$pageHTML = $pageHTML + "<a href='?id=".$id."&page=".($page+2)."'>".($page+2)."</a> ";
+				$pageHTML = $pageHTML."<a href='?id=".$id."&page=".($page+2)."'>".($page+2)."</a> ";
 			}
-			$pageHTML = $pageHTML + "</td></tr>\n";
+			$pageHTML = $pageHTML."</td></tr>\n";
 			echo $pageHTML;
 			echo "\t\t</table><br />\n";
 ?>
