f4387f6ecbcf3cfd6b9eb3602ebe0d6a71681cd5
ExtremeElsanna <Apples292@hotmail.co.uk>
2015-09-09 19:26:06 +0100

    Converted refer to unicode code points

--- search.php
+++ search.php
@@ -1,3 +1,3 @@
 <?php	include("/hdd/elsanna-ssl/scripts/sessionHandler.php");	if (!isset($_POST['search'])) {		$_POST['search'] = "";	} else {		$_POST['search'] = mb_convert_encoding(hex2bin($_POST['search']),'UTF-8','UCS-2');
-		echo $_POST['search';
+		echo $_POST['search'];
 		die;	}	header("Location: /?search=".$_POST['search']);?>
