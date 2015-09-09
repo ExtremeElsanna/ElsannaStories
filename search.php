24f001c84f775efe804d75f1697d6276016d97eb
ExtremeElsanna <Apples292@hotmail.co.uk>
2015-09-09 19:27:31 +0100

    Converted refer to unicode code points

--- search.php
+++ search.php
@@ -1,1 +1,1 @@
-<?php	include("/hdd/elsanna-ssl/scripts/sessionHandler.php");	if (!isset($_POST['search'])) {		$_POST['search'] = "";	} else {		$_POST['search'] = bin2hex(mb_convert_encoding($headerRefer, 'UCS-2', 'UTF-8'));	}	header("Location: /?search=".$_POST['search']);?>
+<?php	include("/hdd/elsanna-ssl/scripts/sessionHandler.php");	if (!isset($_POST['search'])) {		$_POST['search'] = "";	} else {		$_POST['search'] = bin2hex(mb_convert_encoding($_POST['search'], 'UCS-2', 'UTF-8'));	}	header("Location: /?search=".$_POST['search']);?>
