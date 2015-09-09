267ac1fcb104a25b8e02ece2932faa4cf4c6568d
ExtremeElsanna <Apples292@hotmail.co.uk>
2015-09-09 19:10:48 +0100

    Converted refer to unicode code points

--- classes/header.php
+++ classes/header.php
@@ -10,7 +10,7 @@
 	if (!isset($headerRefer)) {
 		$headerRefer = "/";
 	}
-	$headerRefer = bin2hex(iconv('UTF-8','UCS-2', $headerRefer));
+	$headerRefer = bin2hex(mb_convert_encoding($headerRefer, 'UCS-2', 'UTF-8'));
 	// Print either Hi %User% or Hi Guest, with logout and login respectively which will refer back to given link when visited
 	if ($_SESSION['loggedIn'] == 1) {
 		echo "\t\tHi ".$_SESSION['username']."! <a href='/logout/?refer=".$headerRefer."'>Logout</a> - <a href='/user/".$_SESSION['username']."'>Profile</a><br />\n";
