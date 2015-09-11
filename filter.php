4907d0ea90cc14b70dd41545eafbf983d10953fc
ExtremeElsanna <Apples292@hotmail.co.uk>
2015-09-11 21:27:30 +0100

    Filter debug

--- filter.php
+++ filter.php
@@ -193,5 +193,5 @@
 		$getParams .= "sSmut=".toNumbers(mb_substr($smutLevel, 0, -1, "UTF-8"));
 	}
 	$getParams = mb_substr($getParams, 0, -1, "UTF-8");
-	header("Location: /?".$getParams);
+	header("Location: /?a=1&".$getParams);
 ?>
