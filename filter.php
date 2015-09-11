0952b7eb2449b9f840caaa42755ac4074437131b
ExtremeElsanna <Apples292@hotmail.co.uk>
2015-09-11 20:31:16 +0100

    Filter debug

--- filter.php
+++ filter.php
@@ -6,24 +6,24 @@
 		$encParam = bin2hex(mb_convert_encoding($param, 'UCS-2', 'UTF-8'));
 		switch ($key) {
 			case "Title":
-				$getParams = $getParams."sTitle=".$encParam."&"
+				$getParams = $getParams."sTitle=".$encParam."&";
 				break;
 			case "Author":
-				$getParams = $getParams."sAuthor=".$encParam."&"
+				$getParams = $getParams."sAuthor=".$encParam."&";
 				break;
 			case "Length":
-				$getParams = $getParams."sLength=".$encParam."&"
+				$getParams = $getParams."sLength=".$encParam."&";
 				break;
 			case "ElsaCharcter":
-				$getParams = $getParams."sEChar=".$encParam."&"
+				$getParams = $getParams."sEChar=".$encParam."&";
 				break;
 			case "AnnaCharcter":
-				$getParams = $getParams."sAChar=".$encParam."&"
+				$getParams = $getParams."sAChar=".$encParam."&";
 				break;
 			case "DayPublished":
 				if (isset($_POST['MonthPublished']) and isset($_POST['YearPublished'])) {
 					$encParam = strtotime ( $_POST['DayPublished']."/".$_POST['MonthPublished']."/".$_POST['YearPublished']);
-					$getParams = $getParams."sDate=".$encParam."&"
+					$getParams = $getParams."sDate=".$encParam."&";
 				}
 				break;
 		}
