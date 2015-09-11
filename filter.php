1bfeed78cc93009a8a2e3e23e31567cfdfbddeed
ExtremeElsanna <Apples292@hotmail.co.uk>
2015-09-11 21:05:13 +0100

    Filter debug

--- filter.php
+++ filter.php
@@ -47,9 +47,13 @@
 				}
 				break;
 			case "DayPublished":
-				if (isset($_POST['MonthPublished']) and isset($_POST['YearPublished'])) {
-					$encParam = toNumbers(strtotime ( $_POST['DayPublished']."/".$_POST['MonthPublished']."/".$_POST['YearPublished']));
-					$getParams = $getParams."sDate=".$encParam."&";
+				if (isset($_POST['MonthPublished']) and isset($_POST['YearPublished']) and is_numeric($_POST['DayPublished']) and is_numeric($_POST['MonthPublished') and is_numeric($_POST['YearPublished'])) {
+					try {
+						$encParam = toNumbers(strtotime ( $_POST['DayPublished']."/".$_POST['MonthPublished']."/".$_POST['YearPublished']));
+						$getParams = $getParams."sDate=".$encParam."&";
+					} catch (Exception $e) {
+						// Not a valid date
+					}
 				}
 				break;
 			/* START OF THE CHECK BOX OPTIONS */
