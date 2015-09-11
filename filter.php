85423ebf31de848d38eb61c57600c7e0f39a2d94
ExtremeElsanna <Apples292@hotmail.co.uk>
2015-09-11 20:19:53 +0100

    Filter debug

--- filter.php
+++ filter.php
@@ -1,12 +1,9 @@
 <?php
 	include("/hdd/elsanna-ssl/scripts/sessionHandler.php");
 
-	if (!isset($_POST['search'])) {
-		$_POST['search'] = "";
-	} else {
-		set_error_handler(function() { /* ignore errors */ });
-		$_POST['search'] = bin2hex(mb_convert_encoding($_POST['search'], 'UCS-2', 'UTF-8'));
-		restore_error_handler();
+	foreach ($_POST as $param) {
+		echo $param;
 	}
+	die;
 	header("Location: /?search=".$_POST['search']);
 ?>
