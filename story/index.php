49d9e8f1c3cd4c1998c225e3347f8b20d10dc4ee
ExtremeElsanna <Apples292@hotmail.co.uk>
2016-07-31 17:19:10 +0100

    Review rating

--- story/index.php
+++ story/index.php
@@ -428,7 +428,7 @@ if ($story["StoryId"] == "") {
 					if ($row['UserId'] != "") {
 						$username = $row['Username'];
 						
-						echo $review['IncludeRating'];
+						echo $review['UserId'];
 						if ($review['IncludeRating'] == 1)
 						{
 							$stmt = $pdo->prepare('SELECT Rating FROM Ratings WHERE UserId = :userId AND StoryId = :storyId;');
